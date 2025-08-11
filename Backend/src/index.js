d72b311a2fa0de63301bd50acdb51f1a11cfc51d
Karan8100 <upadhyaykaran135@gmail.com>
2025-08-11 17:20:03 +0530

    hello

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,10 +5,12 @@ import { connectDB } from "./config/db.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
+import errorHandler from "./middleware/errorHandler.js";
 
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/user.routes.js";
 import paymentRoutes from "./routes/payments.routes.js";
+import facilityRoutes from "./routes/facilityRoutes.js";
 // import venueRoutes from "./routes/venueRoutes.js";
 // import courtRoutes from "./routes/courtRoutes.js";
 // import bookingRoutes from "./routes/bookingRoutes.js";
@@ -17,6 +19,7 @@ import paymentRoutes from "./routes/payments.routes.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4001;
+connectDB();
 
 app.use(
   cors({
@@ -32,6 +35,7 @@ app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-enco
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/payments", paymentRoutes);
+app.use("/api/facilities", facilityRoutes);
 // app.use("/api/venues", venueRoutes);
 // app.use("/api/courts", courtRoutes);
 // app.use("/api/bookings", bookingRoutes);
