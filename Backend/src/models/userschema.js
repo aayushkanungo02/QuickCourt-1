fb31116a7685d40668edab6496b98fe378a86012
aayushkanungo02 <aayushkanungo02@gmail.com>
2025-08-11 15:05:50 +0530

    update backend

--- Backend/src/models/userschema.js
+++ Backend/src/models/userschema.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["User", "Facility Owner", "Admin"],
+      enum: ["user", "Facility Owner", "Admin"],
       default: "User",
     },
     otp: {
