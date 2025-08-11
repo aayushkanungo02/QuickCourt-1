3c7905863d54253f5d415ba34ab570dd0125d7ac
Disha Mittal <dishamittalnitb@gmail.com>
2025-08-11 10:25:41 +0530

    shadcn-ui_addition_in_frontend_disha

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return <h1 className="text-3xl font-bold underline bg-red-500">Welcome to Karan's repo</h1>;
+  return (
+    <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button>Button</Button>
+    </div>
+  )
 }
 
 export default App
